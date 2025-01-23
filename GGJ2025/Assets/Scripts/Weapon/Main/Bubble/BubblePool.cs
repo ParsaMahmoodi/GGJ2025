6f072171235278995fd13f69d4b0f16f5e35c13b
SinaKarimi <Sinakarimi0938@gmaiol.com>
2025-01-23 16:06:03 +0330

    Create bubblePool

---
+++ GGJ2025/Assets/Scripts/Weapon/Main/Bubble/BubblePool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Weapon.Main.Bubble
+{
+    public class BubblePool
+    {
+        private readonly GameObject _bubblePrefab;
+        private readonly List<GameObject> _pool;
+
+        public BubblePool(GameObject bubblePrefab, int initialSize)
+        {
+            _bubblePrefab = bubblePrefab;
+            _pool = new List<GameObject>(initialSize);
+
+            for (var i = 0; i < initialSize; i++)
+            {
+                var bubble = Object.Instantiate(bubblePrefab);
+                bubble.SetActive(false);
+                _pool.Add(bubble);
+            }
+        }
+
+        public GameObject GetBubble()
+        {
+            foreach (var bubble in _pool)
+            {
+                if (!bubble.activeInHierarchy)
+                {
+                    return bubble;
+                }
+            }
+
+            var newBubble = Object.Instantiate(_bubblePrefab);
+            newBubble.SetActive(false);
+            _pool.Add(newBubble);
+            return newBubble;
+        }
+    }
+}
