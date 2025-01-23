9df01286f7fa9c81b8bb2cc0f77570c33668212b
SinaKarimi <Sinakarimi0938@gmaiol.com>
2025-01-23 16:06:22 +0330

    Create bubble weapon

---
+++ GGJ2025/Assets/Scripts/Weapon/Main/Bubble/BubbleWeapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Weapon.Application;
+
+namespace Weapon.Main.Bubble
+{
+    public class BubbleWeapon : IWeapon
+    {
+        private readonly PlayerManager _playerManager;
+        private readonly BubblePool _bubblePool;
+
+        public BubbleWeapon(PlayerManager playerManager)
+        {
+            _playerManager = playerManager;
+            _bubblePool = new BubblePool(playerManager.bubblePrefab, 10);
+        }
+
+        public void Shoot()
+        {
+        }
+    }
+}
