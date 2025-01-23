301ddba5de39040bd82e957c90c1f8374b196101
SinaKarimi <Sinakarimi0938@gmaiol.com>
2025-01-23 16:05:31 +0330

    Use player data in playe manager

--- GGJ2025/Assets/Scripts/Player/PlayerManager.cs
+++ GGJ2025/Assets/Scripts/Player/PlayerManager.cs
@@ -1,57 +1,57 @@
 using UnityEngine;
 using Weapon.Application;
-using Weapon.Main.Bubble;
 
-public class PlayerManager : MonoBehaviour
+namespace Weapon.Main.Bubble
 {
-    public float moveSpeed = 5f;
-    public Vector2 minBounds;
-    public Vector2 maxBounds;
-
-    private IWeapon _currentWeapon;
-
-    public float initialSize = 1f;
-    public float sizeReductionPerShot = 0.1f;
-    public float minSize = 0.5f;
-    public GameObject bubblePrefab;
-    public Transform shootPoint;
-    public float bubbleShootForce;
-    public int Health;
-
-    private Rigidbody2D _rb;
-    private Vector2 _movement;
-    private Vector3 _currentSize;
-
-    private void Start()
+    public class PlayerManager : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _currentSize = new Vector3(initialSize, initialSize, 1f);
-        transform.localScale = _currentSize;
+        public GameObject bubblePrefab;
+        public Transform shootPoint;
 
-        _currentWeapon = new BubbleWeapon(this);
-    }
+        private IWeapon _currentWeapon;
+        private Rigidbody2D _rb;
+        private Vector2 _movement;
+        private Vector3 _currentSize;
+        private float _health;
+        private PlayerData _playerData;
 
-    private void Update()
-    {
-        _movement.x = Input.GetAxisRaw("Horizontal");
-        _movement.y = Input.GetAxisRaw("Vertical");
-        _movement = _movement.normalized;
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        private void Start()
         {
-            _currentWeapon.Shoot();
+            ResolveDependency();
+            _rb = GetComponent<Rigidbody2D>();
+            _currentSize = new Vector3(_playerData.initialSize, _playerData.initialSize, 1f);
+            transform.localScale = _currentSize;
+
+            _currentWeapon = new BubbleWeapon(this);
         }
-    }
 
-    private void FixedUpdate()
-    {
-        _rb.velocity = _movement * moveSpeed;
+        private void ResolveDependency()
+        {
+            _playerData = Resources.Load<PlayerData>("Player/PlayerData");
+        }
 
-        var clampedPosition = new Vector2(
-            Mathf.Clamp(_rb.position.x, minBounds.x, maxBounds.x),
-            Mathf.Clamp(_rb.position.y, minBounds.y, maxBounds.y)
-        );
+        private void Update()
+        {
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.y = Input.GetAxisRaw("Vertical");
+            _movement = _movement.normalized;
 
-        _rb.position = clampedPosition;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _currentWeapon.Shoot();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = _movement * _playerData.moveSpeed;
+
+            var clampedPosition = new Vector2(
+                Mathf.Clamp(_rb.position.x, _playerData.minBounds.x, _playerData.maxBounds.x),
+                Mathf.Clamp(_rb.position.y, _playerData.minBounds.y, _playerData.maxBounds.y)
+            );
+
+            _rb.position = clampedPosition;
+        }
     }
 }
