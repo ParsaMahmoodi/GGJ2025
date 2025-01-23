263a4e186b9e93d235e710d10d7eeca790f1975e
SinaKarimi <Sinakarimi0938@gmaiol.com>
2025-01-23 16:05:20 +0330

    Create player data

---
+++ GGJ2025/Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Weapon.Main.Bubble
+{
+    [CreateAssetMenu(menuName = "Game/Player/Player data", fileName = "NewPlayerData")]
+    public class PlayerData : ScriptableObject
+    {
+        public Vector2 minBounds;
+        public Vector2 maxBounds;
+        public float moveSpeed = 5f;
+        public float bubbleShootForce;
+        public float initialSize = 1f;
+        public float sizeReductionPerShot = 0.1f;
+        public float minSize = 0.5f;
+        public float initialHealth;
+    }
+}
