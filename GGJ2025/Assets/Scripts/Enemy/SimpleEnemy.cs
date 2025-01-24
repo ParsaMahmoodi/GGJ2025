c318cbf996ef1742bec45d1d60bb60dbf1eb441a
SaraBre <Sarabre26@gmail.com>
2025-01-24 09:02:27 +0330

    Implement Killing Behavior

--- GGJ2025/Assets/Scripts/Enemy/SimpleEnemy.cs
+++ GGJ2025/Assets/Scripts/Enemy/SimpleEnemy.cs
@@ -1,5 +1,13 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SimpleEnemy : Enemy
 {
+    public Spawner FishSpawner;
+    
+    public override void Kill()
+    {
+        base.Kill();
+        FishSpawner.Spawn(transform.position,quaternion.identity);
+    }
 }
