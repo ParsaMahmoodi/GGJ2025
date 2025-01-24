527d746b04a8b1e3fbf556ccb4fee15d0fdc6220
SaraBre <Sarabre26@gmail.com>
2025-01-24 12:11:24 +0330

    consolidate fish spawning logic into a single spawner

--- GGJ2025/Assets/Scripts/Enemy/SimpleEnemy.cs
+++ GGJ2025/Assets/Scripts/Enemy/SimpleEnemy.cs
@@ -5,9 +5,14 @@ public class SimpleEnemy : Enemy
 {
     public Spawner FishSpawner;
     public Health Health;
+
+    private string FishSpawnerTag = "FishSpawner";
+   
+    
     protected override void Init()
     {
         base.Init();
+        FishSpawner = GameObject.FindGameObjectWithTag(FishSpawnerTag).GetComponent<Spawner>();
         Health.OnDie += Kill;
         Health.OnDie += SpawnFish;
     }
