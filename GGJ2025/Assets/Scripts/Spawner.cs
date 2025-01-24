6d8090ffa79393081b711a5a2d2885f054dfbe25
SinaKarimi <Sinakarimi0938@gmaiol.com>
2025-01-24 17:30:50 +0330

    Sprites

--- GGJ2025/Assets/Scripts/Spawner.cs
+++ GGJ2025/Assets/Scripts/Spawner.cs
@@ -10,42 +10,43 @@ public class Spawner : MonoBehaviour
     public int InitialSize = 10;
     public float queueTime = 1.5f;
     public bool ShouldSpawnOnUpdate;
-    
+
     private ObjectPool _pool;
     private float _time = 0;
 
     private void Start()
     {
         int initialSize = InitialSize * SpawnCountPerFrame;
-        _pool = new ObjectPool(_object,initialSize);
+        _pool = new ObjectPool(_object, initialSize);
     }
 
     private void Update()
     {
-        if(!ShouldSpawnOnUpdate)
+        if (!ShouldSpawnOnUpdate)
             return;
-        
+
         if (_time > queueTime)
         {
             Spawn();
             _time = 0;
         }
+
         _time += Time.deltaTime;
     }
 
     public void Spawn()
     {
         for (int i = 0; i < SpawnCountPerFrame; i++)
-        { 
+        {
             Instantiate();
         }
     }
-    
-    public void Spawn(Vector3 position,Quaternion rotation)
+
+    public void Spawn(Vector3 position, Quaternion rotation)
     {
         for (int i = 0; i < SpawnCountPerFrame; i++)
         {
-            Instantiate(position,rotation);
+            Instantiate(position, rotation);
         }
     }
 
@@ -54,13 +55,13 @@ public class Spawner : MonoBehaviour
         GameObject obj = _pool.GetObject();
         obj.SetActive(true);
         return obj;
-    } 
-    
-    public virtual GameObject Instantiate(Vector3 position,Quaternion rotation)
+    }
+
+    public virtual GameObject Instantiate(Vector3 position, Quaternion rotation)
     {
         GameObject obj = Instantiate();
         obj.transform.position = position;
         obj.transform.rotation = rotation;
         return obj;
-    } 
+    }
 }
