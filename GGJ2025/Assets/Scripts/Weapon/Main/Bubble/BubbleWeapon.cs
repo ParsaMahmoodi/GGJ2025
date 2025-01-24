ddd3f08273ba0e89b325e9959dbef6b9bcbcda2d
SaraBre <Sarabre26@gmail.com>
2025-01-24 11:20:40 +0330

    Refactor pool structure to support bullet pool

--- GGJ2025/Assets/Scripts/Weapon/Main/Bubble/BubbleWeapon.cs
+++ GGJ2025/Assets/Scripts/Weapon/Main/Bubble/BubbleWeapon.cs
@@ -6,12 +6,12 @@ namespace Weapon.Main.Bubble
     public class BubbleWeapon : IWeapon
     {
         private readonly PlayerManager _playerManager;
-        private readonly BubblePool _bubblePool;
+        private readonly ObjectPool _bubblePool;
 
         public BubbleWeapon(PlayerManager playerManager)
         {
             _playerManager = playerManager;
-            _bubblePool = new BubblePool(playerManager.bubblePrefab, 10);
+            _bubblePool = new ObjectPool(playerManager.bubblePrefab, 10);
         }
 
         public void Shoot()
